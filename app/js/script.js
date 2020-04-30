e2ef07f58b6a8e267ef642d1818840c5300e45b7
Nikita <nikitos5153@gmail.com>
2020-04-30 14:53:20 +0300

    final version

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,9 @@
 var burgerMenu = document.querySelector('.site-header__burger');
 var header = document.querySelector('.site-header');
+var body = document.querySelector('body');
 
 burgerMenu.addEventListener('click', function () {
     header.classList.toggle('site-header--opened');
     burgerMenu.classList.toggle('site-header__burger--opened')
+    body.classList.toggle('fixed');
 })
